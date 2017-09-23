f41398bb5b116f5fa6da03a1f1852594c849739b
akashin <kashin.andrej@gmail.com>
2017-09-23 19:53:25 +0300

    Add code for measuring performance of DQN

--- baselines/multi_deepq/experiments/custom_cartpole.py
+++ baselines/multi_deepq/experiments/custom_cartpole.py
@@ -13,6 +13,7 @@ import baselines.common.tf_util as U
 
 from baselines import logger
 from baselines import multi_deepq
+from baselines.multi_deepq import wrappers
 from baselines.multi_deepq.replay_buffer import ReplayBuffer
 from baselines.common.schedules import LinearSchedule
 
@@ -30,13 +31,13 @@ class Worker(object):
     def __init__(self, is_chief, env, model):
         self.is_chief = is_chief
         self.env = env
-        self.act, self.train, self.update_target, self.debug = multi_deepq.build_train(
-                make_obs_ph=lambda name: U.BatchInput(env.observation_space.shape, name=name),
-                q_func=model,
-                num_actions=env.action_space.n,
-                optimizer=tf.train.AdamOptimizer(learning_rate=5e-4),
-                reuse=(not is_chief),
-                )
+        # self.act, self.train, self.update_target, self.debug = multi_deepq.build_train(
+                # make_obs_ph=lambda name: U.BatchInput(env.observation_space.shape, name=name),
+                # q_func=model,
+                # num_actions=env.action_space.n,
+                # optimizer=tf.train.AdamOptimizer(learning_rate=5e-4),
+                # reuse=(not is_chief),
+                # )
 
         # Create the replay buffer
         self.replay_buffer = ReplayBuffer(50000)
@@ -51,7 +52,8 @@ class Worker(object):
         start_time = timer()
         for t in itertools.count():
             # Take action and update exploration to the newest value
-            action = self.act(obs[None], update_eps=self.exploration.value(t), session=session)[0]
+            # action = self.act(obs[None], update_eps=self.exploration.value(t), session=session)[0]
+            action = 0
             new_obs, rew, done, _ = self.env.step(action)
             # Store transition in the replay buffer.
             self.replay_buffer.add(obs, action, rew, new_obs, float(done))
@@ -62,46 +64,82 @@ class Worker(object):
                 obs = self.env.reset()
                 episode_rewards.append(0)
 
-            is_solved = t > 100 and np.mean(episode_rewards[-101:-1]) >= 200
+            is_solved = len(episode_rewards) > 100 and np.mean(episode_rewards[-101:-1]) >= 200
             if self.is_chief and is_solved:
                 # Show off the result
                 self.env.render()
             else:
                 # Minimize the error in Bellman's equation on a batch sampled from replay buffer.
-                if t > 1000:
+                # if t > 1000:
+                if False:
                     obses_t, actions, rewards, obses_tp1, dones = self.replay_buffer.sample(args.batch_size)
                     self.train(obses_t, actions, rewards, obses_tp1, dones, np.ones_like(rewards),
                             session=session)
                     # Update target network periodically.
-                if self.is_chief and t % 250 == 0:
+                # if self.is_chief and t % 250 == 0:
+                if False:
                     self.update_target(session=session)
 
-            if self.is_chief and done and len(episode_rewards) % 10 == 0:
+            if self.is_chief and done and len(episode_rewards) % 100 == 0:
                 logger.record_tabular("steps", t)
                 logger.record_tabular("episodes", len(episode_rewards))
                 logger.record_tabular("mean episode reward", round(np.mean(episode_rewards[-101:-1]), 1))
                 logger.record_tabular("time elapsed", timer() - start_time)
+                logger.record_tabular("steps/s", t / (timer() - start_time))
                 logger.record_tabular("% time spent exploring", int(100 * self.exploration.value(t)))
                 logger.dump_tabular()
 
 
-def make_env(seed):
+class StupidWorker(object):
+    def __init__(self, is_chief, env, model):
+        self.env = env
+        self.is_chief = is_chief
+
+    def run(self, session, coord):
+        _ = self.env.reset()
+
+        start_time = timer()
+        for t in itertools.count():
+            # Take action and update exploration to the newest value
+            # action = self.act(obs[None], update_eps=self.exploration.value(t), session=session)[0]
+            action = 0
+            _, _, done, _ = self.env.step(action)
+            if done:
+                obs = self.env.reset()
+
+            if self.is_chief and t % 100000 == 0:
+                logger.record_tabular("steps", t)
+                logger.record_tabular("time elapsed", timer() - start_time)
+                logger.record_tabular("steps/s", t / (timer() - start_time))
+                logger.dump_tabular()
+
+
+def make_env(env_name, seed):
     # Create the environment
-    env = gym.make("CartPole-v0")
-    env.seed(seed)
-    return env
+    def make_gym_env():
+        env = gym.make(env_name)
+        env.seed(seed)
+        return env
+
+    return make_gym_env()
+    # return wrappers.ExternalProcess(make_gym_env)
+
+
+MODE = 'no_training'
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('--batch_size', help='Batch size', type=int, default=32)
     parser.add_argument('--worker_count', help='Batch size', type=int, default=1)
+    parser.add_argument('--env_name', help='Batch size', type=str, default='CartPole-v0')
     args = parser.parse_args()
 
     np.random.seed(42)
     tf.set_random_seed(7)
 
-    log_dir = "./results/multi_deepq_{}".format(args.batch_size)
+    log_dir = "./results/multi_deepq_{}_{}_batch_size_{}_worker_count_{}".format(
+            MODE, args.env_name, args.batch_size, args.worker_count)
     logger.configure(dir=log_dir)
     print("Running training with arguments: {} and log_dir: {}".format(args, log_dir))
 
@@ -109,7 +147,8 @@ if __name__ == '__main__':
 
     workers = []
     for i in range(args.worker_count):
-        workers.append(Worker(i == 0, make_env(i), linear_model))
+        workers.append(Worker(i == 0, make_env(args.env_name, i), linear_model))
+        # workers.append(StupidWorker(i == 0, make_env(args.env_name, i), linear_model))
 
     with U.make_session(1) as session:
         U.initialize(session=session)
