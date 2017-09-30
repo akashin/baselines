d0c0ef259c733c41e88d7c05f3100b563cc7114b
akashin <kashin.andrej@gmail.com>
2017-09-30 13:55:38 +0100

    Add script for installing dependencies

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# git clone https://github.com/akashin/baselines.git
+
+sudo /anaconda/envs/py35/bin/pip install dill plumbum gym
+sudo apt install -y python-numpy cmake zlib1g-dev libjpeg-dev libboost-all-dev gcc libsdl2-dev wget unzip
+sudo /anaconda/envs/py35/bin/pip install ppaquette-gym-doom
+
