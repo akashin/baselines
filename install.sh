2a318c46971f46fb57f656712c04818a04114606
akashin <kashin.andrej@gmail.com>
2017-10-07 20:44:52 +0100

    Update install script

--- install.sh
+++ install.sh
@@ -2,7 +2,8 @@
 
 # git clone https://github.com/akashin/baselines.git
 
-sudo /anaconda/envs/py35/bin/pip install dill plumbum gym
+sudo /anaconda/envs/py35/bin/pip install dill plumbum gym --upgrade
+sudo /anaconda/envs/py35/bin/pip gym[atari] --upgrade
 sudo apt install -y python-numpy cmake zlib1g-dev libjpeg-dev libboost-all-dev gcc libsdl2-dev wget unzip
 sudo /anaconda/envs/py35/bin/pip install ppaquette-gym-doom
 
